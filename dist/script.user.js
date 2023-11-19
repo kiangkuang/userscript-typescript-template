5fcb313df752bf65631027a39d371c9dad20d764
Kiang Kuang <kiangkuang@gmail.com>
2023-11-20 01:17:11 +0800

    feat: add commonly used headers

--- dist/script.user.js
+++ dist/script.user.js
@@ -1,8 +1,11 @@
 // ==UserScript==
-// @name       userscript-name
-// @namespace  https://github.com/kiangkuang
-// @version    0.0.1
-// @author     Kiang Kuang
+// @name         userscript-name
+// @namespace    https://github.com/kiangkuang
+// @version      0.0.1
+// @author       Kiang Kuang
+// @description  userscript description
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @match        https://github.com/*
 // ==/UserScript==
 
 (function () {
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ const userscript: MonkeyUserScript = {
   name: "userscript-name",
   namespace: "https://github.com/kiangkuang",
   version: "0.0.1",
+  description: "userscript description",
   author: "Kiang Kuang",
+  match: "https://github.com/*",
+  icon: "https://www.google.com/s2/favicons?sz=64&domain=github.com",
+  grant: [],
+  require: [],
   // add any other userscript headers
 };
 
